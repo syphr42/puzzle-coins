aae82dca4175d7a8dce707fd87dba41ae2cf320e
Gregory Moyer <moyerg@syphr.com>
2018-03-18 10:18:53 -0400

    Provide a Coin and a Scale
    Signed-off-by: Gregory Moyer <moyerg@syphr.com>

---
+++ src/main/java/org/syphr/puzzle/coins/Coin.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright 2018 Gregory Moyer and contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.syphr.puzzle.coins;
+
+public class Coin
+{
+    private final int weight;
+
+    public Coin(int weight)
+    {
+        this.weight = weight;
+    }
+
+    public int getWeight()
+    {
+        return weight;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + weight;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+        if (obj == null)
+        {
+            return false;
+        }
+        if (!(obj instanceof Coin))
+        {
+            return false;
+        }
+        Coin other = (Coin)obj;
+        if (weight != other.weight)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString()
+    {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Coin [weight=");
+        builder.append(weight);
+        builder.append("]");
+        return builder.toString();
+    }
+}
---
+++ src/test/java/org/syphr/puzzle/coins/ScaleTest.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright 2018 Gregory Moyer and contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.syphr.puzzle.coins;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class ScaleTest
+{
+    static Scale scale;
+
+    @BeforeAll
+    static void beforeAll()
+    {
+        scale = new Scale();
+    }
+
+    @Test
+    void returnsPositiveNumberComparedToSmaller_SingleCoins()
+    {
+        assertTrue(scale.compare(coin(2), coin(1)) > 0);
+    }
+
+    @Test
+    void returnsNegativeNumberComparedToLarger_SingleCoins()
+    {
+        assertTrue(scale.compare(coin(1), coin(2)) < 0);
+    }
+
+    @Test
+    void returnsZeroComparedToEqual_SingleCoins()
+    {
+        assertTrue(scale.compare(coin(1), coin(1)) == 0);
+    }
+
+    @Test
+    void returnsPositiveNumberComparedToSmaller_Collections()
+    {
+        assertAll(() -> assertTrue(scale.compare(coins(2), coins(1)) > 0),
+                  () -> assertTrue(scale.compare(coins(1, 1), coins(1)) > 0),
+                  () -> assertTrue(scale.compare(coins(3), coins(1, 1)) > 0),
+                  () -> assertTrue(scale.compare(coins(1, 2, 3), coins(2, 2, 1)) > 0),
+                  () -> assertTrue(scale.compare(coins(1, 2, 3), coins(1, 2)) > 0),
+                  () -> assertTrue(scale.compare(coins(4, 5), coins(1, 2, 3)) > 0));
+    }
+
+    @Test
+    void returnsNegativeNumberComparedToLarger_Collections()
+    {
+        assertAll(() -> assertTrue(scale.compare(coins(1), coins(2)) < 0),
+                  () -> assertTrue(scale.compare(coins(1), coins(1, 1)) < 0),
+                  () -> assertTrue(scale.compare(coins(1, 1), coins(3)) < 0),
+                  () -> assertTrue(scale.compare(coins(2, 2, 1), coins(1, 2, 3)) < 0),
+                  () -> assertTrue(scale.compare(coins(1, 2), coins(1, 2, 3)) < 0),
+                  () -> assertTrue(scale.compare(coins(1, 2, 3), coins(4, 5)) < 0));
+    }
+
+    @Test
+    void returnsZeroComparedToEqual_Collections()
+    {
+        assertAll(() -> assertTrue(scale.compare(coins(1), coins(1)) == 0),
+                  () -> assertTrue(scale.compare(coins(2), coins(1, 1)) == 0),
+                  () -> assertTrue(scale.compare(coins(1, 2), coins(3)) == 0),
+                  () -> assertTrue(scale.compare(coins(3, 2, 1), coins(1, 2, 3)) == 0),
+                  () -> assertTrue(scale.compare(coins(4, 2), coins(1, 2, 3)) == 0),
+                  () -> assertTrue(scale.compare(coins(3, 3, 3), coins(4, 5)) == 0));
+    }
+
+    Coin coin(int weight)
+    {
+        return new Coin(weight);
+    }
+
+    List<Coin> coins(int... weights)
+    {
+        return Arrays.stream(weights).mapToObj(weight -> coin(weight)).collect(Collectors.toList());
+    }
+}
